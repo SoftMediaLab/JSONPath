21f72e778e59f4fe0b465c13f3d1a80624ccd14d
godba11 <nkhudkov@softmedialab.com>
2020-11-05 16:30:40 +0500

    fix global require

--- jsonpath.js
+++ jsonpath.js
@@ -1,5 +1,7 @@
 /*! jsonpath 1.0.2 */
 
+var _uniq = require('underscore').uniq;
+
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.jsonpath = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({"./aesprim":[function(require,module,exports){
 /*
   Copyright (C) 2013 Ariya Hidayat <ariya.hidayat@gmail.com>
@@ -4603,7 +4605,6 @@ module.exports = grammar;
 var aesprim = require('./aesprim');
 var slice = require('./slice');
 var _evaluate = require('static-eval');
-var _uniq = require('underscore').uniq;
 
 var Handlers = function() {
   return this.initialize.apply(this, arguments);
